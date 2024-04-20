fa1a6f44e3fe6d11a1f8ddb1bb4fafce7265d90a
RaysWill <156453048+RaysWill@users.noreply.github.com>
2024-04-20 07:21:02 -0400

    Update Plots.py

--- Plots.py
+++ Plots.py
@@ -16,15 +16,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 
+import seaborn as sns
+# The following is the link to the datasets: https://www.inpredictable.com/2021/01/nba-player-shooting-motions-data-dump.html 
 # The code below is used to read the datasets and save them to be able to accessed/referenced in the code later on.
-cwd = os.getcwd() 
-print(cwd)
+
 file_path = 'C:/Users/chess/OneDrive/Desktop/J.I.S Project/path_detail.csv'
 file_path_made = 'C:/Users/chess/OneDrive/Desktop/J.I.S Project/path_detail_made.csv'
 file_path_miss = 'C:/Users/chess/OneDrive/Desktop/J.I.S Project/path_detail_miss.csv'
 data = pd.read_csv(file_path)
 
-
+# Data sets split for 
 data_made = pd.read_csv(file_path_made)
 data_miss = pd.read_csv(file_path_miss)
 
@@ -38,11 +39,11 @@ data_miss['made'] = 0
 
 # These functions below allows you to view and look at the different parts of the dataset
 
-# print(data.head())
-# print(data.info())
-# print(data.describe())
-# print(data_miss.info())
-# print(data_made.info())
+print(data.head())
+print(data.info())
+print(data.describe())
+print(data_miss.info())
+print(data_made.info())
 
 
 #These functions are used to clean the datasets by removing and row with missing values and any duplicate values found in the dataset
@@ -52,30 +53,40 @@ print(data_miss.isnull().sum())
 data_miss.dropna(inplace=True)
 data_miss.drop_duplicates(inplace = True)
 
+
 print("Number of Missing Values")
 print(data_made.isnull().sum())
 data_made.dropna(inplace=True)
 data_made.drop_duplicates(inplace = True)
 
 #The Following code is used to combine the cleaned datasets and save them to prevent having to clean and run all the above code everytime.
+data_made.rename(columns = {'cz': 'height'}, inplace=True)
+data_miss.rename(columns = {'cz': 'height'}, inplace=True)
+data_made['power'] = data_made['cv'] * data_made['ca']
+data_miss['power'] = data_miss['cv'] * data_miss['ca']
 
 data_combined = pd.concat([data_made, data_miss], ignore_index= True)
 data_shuffled = data_combined.sample(frac=1, random_state = 56)
+
+
+
+columns_to_remove = ['lnm', 'fnm', 'pid', 'ddst']
+data_shuffled.drop(columns= columns_to_remove, inplace=True)
 data_shuffled.to_csv('combined_shots.csv', index =False)
+
 
 
 
 Shots = pd.read_csv('combined_shots.csv')
 
-Shots_small = pd.read_csv('combined_shots.csv', nrows=100)
-Shots_made = pd.read_csv(file_path_made, nrows=100 )
-print ("tHIS IS TEH mERGED Dataset")
+
+print ("This is the Merged Dataset")
 print(Shots.head())
 print(Shots.tail())
 
 # Scatterplot that shows the differences between 
 plt.figure(figsize=(8,6))
-plt.scatter(Shots_made['t'], Shots_made['cv'])
+plt.scatter(data_made['t'], data_made['cv'])
 plt.colorbar(label = 'Shot Outcome')
 plt.xlabel('Shot Release Time')
 plt.ylabel('Shot Speed')
@@ -85,19 +96,19 @@ plt.show()
 
 # Scatterplot showing the correlation Height and Release Time
 
-# plt.figure(figsize=(8,6))
-# plt.scatter(Shots_made['hght'], Shots_made['rt'])
-# plt.colorbar(label = 'Shot Outcome')
-# plt.xlabel('Shot Release Time')
-# plt.ylabel('Shot Speed')
-# plt.title('Basketball Shots: Distance vs Speed')
-# plt.grid(True)
-# plt.show()
+plt.figure(figsize=(8,6))
+plt.scatter(data_made['height'], data_made['power'])
+plt.colorbar(label = 'Shot Outcome')
+plt.xlabel('Shot Height')
+plt.ylabel('Shot Power')
+plt.title('Basketball Shots: Distance vs Speed')
+plt.grid(True)
+plt.show()
 
 # Scatterplot Showing the correlation between shot speed and acceleration
 
 plt.figure(figsize=(8,6))
-plt.scatter(Shots_made['ca'], Shots_made['cv'])
+plt.scatter(data_made['ca'], data_made['cv'])
 plt.colorbar(label = 'Shot Outcome')
 plt.xlabel('Shot Acceleration')
 plt.ylabel('Shot Speed')
@@ -106,19 +117,18 @@ plt.grid(True)
 plt.show()
 
 # Create a Combined Variable for Velocity and Do a scatterplot for it
-Shots_made['Velocity'] = Shots_made['cvx'] * Shots_made['cvy'] * Shots_made['cvz']
 
-# plt.figure(figsize=(8,6))
-# plt.scatter(Shots_made['Velocity'], Shots_made['rt'])
-# plt.colorbar(label = 'Shot Outcome')
-# plt.xlabel('Shot Velocity')
-# plt.ylabel('Release Time')
-# plt.title('Basketball Shots: Velocity vs Release Time')
-# plt.grid(True)
-# plt.show()
-# Machine Learning Model Creation
 
-# The dataset is Extremly large so this code is implemented to allow you to process the data when training the model one chunk(part at a time)
 
-chunk_size = 10000
-data_set = pd.read_csv('combined_shots.csv', chunksize =chunk_size)
+
+
+
+corr = Shots.corr()
+plt.figure(figsize=(12,9))
+sns.heatmap(corr, annot=True, cmap='YlGnBu')
+plt.savefig('heatmap.png')
+plt.show()
+
+
+print("Matrix")
+
