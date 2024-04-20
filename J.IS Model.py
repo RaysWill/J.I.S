5bc770ffcb9cdc0981dcc68489af1ed7959190c5
RaysWill <156453048+RaysWill@users.noreply.github.com>
2024-04-20 07:07:15 -0400

    Update and rename J.IS Code.py to J.IS Model.py

---
+++ J.IS Model.py
@@ -0,0 +1,131 @@
+import sys
+import os
+
+# The pandas library allows you to import the dataset and allows you to examine, manipulate, and clean the dataset
+import pandas as pd
+# The tensorflow library is neccessary for machine learning but for usually for complex data analysis and deep learning
+os.environ['TF_CPP_MIN_LOG_LEVEL'] ='2'
+import tensorflow as tf
+# The numpy library gives you access to different math and algerba functions and expressions to be used during machine learning
+import numpy as np
+# The matplotlib is used to creaete plots and graphs displaying the information from the dataset
+import matplotlib.pyplot as plt
+# The sklearn library (known as scikit-learn) is the library that provides the most simple ways to perform machine learning and access related functions and resources
+
+import joblib
+
+import sklearn
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.utils.validation import check_is_fitted
+# Import seaborn which is used
+import seaborn as sns
+
+cwd = os.getcwd() 
+print(cwd)
+
+Shots = pd.read_csv('combined_shots.csv')
+
+print ("This is the Merged DataSet")
+print(Shots.head())
+print(Shots.tail())
+
+
+
+
+corr = Shots.corr()
+plt.figure(figsize=(12,9))
+sns.heatmap(corr, annot=True, cmap='YlGnBu')
+plt.savefig('heatmap.png')
+plt.show()
+
+
+#print("Matrix")
+
+chunk_size = 10000
+data_set = pd.read_csv('combined_shots.csv', chunksize =chunk_size)
+
+
+
+#Create the model, You should do a logistic regression when the result is either yes or no and there are linear relationships in the data
+model = LogisticRegression(max_iter = 100000)
+
+
+#Allows you to loop through the different parts of dat_set and allows
+#for i, chunk in enumerate(data_set):
+    #print(f'Processing chunk{i+1}')
+
+    # Split the dataset into different parts with some to be used to train and some being used to test
+    # Also Isolating the target variable to show what your trying to get the model to train based on
+    #column = ['made']
+    #x = chunk.drop(columns = column)
+    #y = chunk['made']
+
+    # Then split the data into different parts for the training and testing
+    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=56)
+
+    #Train the Model
+    #model.fit(x_train, y_train)
+
+    #
+    #prediction = model.predict(x_test)
+    #print(f'Prediction on chunk {i+1}: {prediction}')
+
+    #accuracy = model.score(x_test, y_test)
+    #print(f'Accuracy on chunk {i+1}: {accuracy}')
+
+# The Next Steps 
+
+# 1. Create a Model removing certain uneccesary variables and determine which variables are the most important and comparing it to the accuracy of the original model
+columns_to_drop = ['caz', 'cay', 'cax', 'cvx', 'cvy', 'cvz', 'hght', 'cx', 'cy', 'cv', 'ca', 'dx', 'dy', 'dz', 'd', 'rt' ]
+Shots.drop(columns= columns_to_drop, inplace =True)
+Shots.to_csv('combined_shots_reduced.csv', index =False)
+
+
+
+chunk_size = 10000
+Shots2 = pd.read_csv('combined_shots_reduced.csv', chunksize = chunk_size)
+
+corr = Shots.corr()
+plt.figure(figsize=(12,9))
+sns.heatmap(corr, annot=True, cmap='YlGnBu')
+plt.savefig('heatmap.png')
+plt.show()
+
+
+model2 = LogisticRegression(max_iter= 100000)
+
+#Allows you to loop through the different parts of dat_set and allows
+for i, chunk in enumerate(Shots2):
+    print(f'Processing chunk{i+1}')
+
+    # Split the dataset into different parts with some to be used to train and some being used to test
+    # Also Isolating the target variable to show what your trying to get the model to train based on
+    column = ['made']
+    x = chunk.drop(columns = column)
+    y = chunk['made']
+
+    # Then split the data into different parts for the training and testing
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=56)
+
+    #Train the Model
+    model2.fit(x_train, y_train)
+
+    #
+    prediction = model2.predict(x_test)
+    print(f'Prediction on chunk {i+1}: {prediction}')
+
+    accuracy = model2.score(x_test, y_test)
+    print(f'Accuracy on chunk {i+1}: {accuracy}')
+
+
+file_path = 'trained_model2.pk1'
+joblib.dump(model2, file_path)
+print("Trained model saved to:", file_path)
